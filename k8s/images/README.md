51d0105c876387ce8c838c7cae5372a3e6fbf83c
Michal Koplinger <m.koplinger@gmail.com>
2023-10-16 22:43:40 +0200

    no-18_docker-hub_4P5U5RHr

---
+++ k8s/images/README.md
@@ -0,0 +1,13 @@
+# Usage
+
+```sh
+npm install
+```
+
+```sh
+Usage: node script.js [options]
+
+Options:
+  -h          Display help message
+  -n          Use nerdctl cli instead of docker cli
+```
---
+++ k8s/images/publish.js
@@ -0,0 +1,78 @@
+const { spawn, exec } = require('child_process');
+const { program } = require('commander');
+
+program
+    .option('-h, --help', 'Display help message')
+    .option('-n, --nerdctl', 'Use nerdctl cli instead of docker cli')
+    .parse();
+
+const options = program.opts();
+
+if (options.help) {
+    program.outputHelp();
+    process.exit(0);
+}
+
+const cli = options.nerdctl ? 'nerdctl' : 'docker';
+
+function executeCommand(command, args) {
+    return new Promise((resolve, reject) => {
+        const process = spawn(command, args, { stdio: 'inherit' });
+
+        process.on('exit', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(`Command '${command}' failed with exit code ${code}`);
+            }
+        });
+    });
+}
+
+function getGitCommitHash() {
+    return new Promise((resolve, reject) => {
+        exec('git rev-parse HEAD', (error, stdout, stderr) => {
+            if (error) {
+                reject(error.message);
+                return;
+            }
+            if (stderr) {
+                reject(stderr);
+                return;
+            }
+            resolve(stdout.trim());
+        });
+    });
+}
+
+async function main() {
+    const username = 'pnxedudevops';
+    const repository = 'apps';
+    const gitSHA = await getGitCommitHash();
+    const contextPath = `${__dirname}/../../src/frontend/devops-app`;
+    const latestTag = 'frontend-latest';
+    const gitCommitTag = `frontend-${gitSHA}`
+    const latestImageName = `${username}/${repository}:${latestTag}`;
+    const commitImageName = `${username}/${repository}:${gitCommitTag}`;
+
+    try {
+        console.log(`Running: ${cli} build -t ${latestImageName} -t ${commitImageName} ${contextPath}`);
+        await executeCommand(cli, ['build', '-t', `${latestImageName}`, '-t', `${commitImageName}`, contextPath]);
+        console.log('Docker build successful.');
+
+        console.log(`Logging in to docker hub`);
+        await executeCommand(cli, ['login']);
+
+        console.log(`Running: ${cli} push ${latestImageName}`);
+        await executeCommand(cli, ['push', `${latestImageName}`]);
+        console.log('Docker push successful.');
+
+        console.log(`Running: ${cli} push ${latestImageName}`);
+        await executeCommand(cli, ['push', `${commitImageName}`]);
+        console.log('Docker push successful.');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
